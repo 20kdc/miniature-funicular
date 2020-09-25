94e71c13fc6007a229f70b8eb8606e246065d5cd
20kdc <asdd2808@gmail.com>
2020-09-25 16:27:42 +0100

    moonboot: ignore current directory

--- src/moonboot_dll.c
+++ src/moonboot_dll.c
@@ -6,6 +6,43 @@ typedef void (*openlibs_t)(void * L);
 typedef int (*loadfile_t)(void * L, const char * filename);
 typedef int (*pcall_t)(void * L, int nargs, int nresults, int errfunc);
 
+// Yes, I *could* use UTF-16 here.
+// But Lua *won't*. Hands are kinda tied.
+// Anyway, nobody actually said how long a long path could be, so I'm hardcoding something "sufficiently big"...
+char * findDir() {
+	int pathBufferLen = 512;
+	while (1) {
+		char * buffer = malloc(pathBufferLen);
+		int res = GetModuleFileNameA(NULL, buffer, pathBufferLen);
+		if (res == pathBufferLen) {
+			free(buffer);
+			pathBufferLen *= 2;
+		} else {
+			buffer = realloc(buffer, res + 1);
+			for (int i = res; i >= 0; i--) {
+				char ch = buffer[i];
+				if ((ch == '\\') || (ch == '/')) {
+					buffer[i + 1] = 0;
+					buffer = realloc(buffer, i + 2);
+					return buffer;
+				}
+			}
+			// no directory, realloc to empty string
+			buffer = realloc(buffer, 1);
+			buffer[0] = 0;
+			return buffer;
+		}
+	}
+}
+
+char * concat(const char * a, const char * b) {
+	size_t lenA = strlen(a);
+	char * fin = malloc(lenA + strlen(b) + 1);
+	strcpy(fin, a);
+	strcpy(fin + lenA, b);
+	return fin;
+}
+
 void luaboot() {
 	void * dll = LoadLibraryA("lua51");
 	if (!dll) {
@@ -20,10 +57,14 @@ void luaboot() {
 
 	void * L = luaL_newstate();
 	luaL_openlibs(L);
-	if (luaL_loadfile(L, "moonboot/bootloader.lua")) {
+	char * dir = findDir();
+	char * boot = concat(dir, "moonboot/bootloader.lua");
+	free(dir);
+	if (luaL_loadfile(L, boot)) {
 		MessageBoxA(NULL, "moonboot failed to load 'moonboot/bootloader.lua'", "moonboot diagnostic", 0);
 		ExitProcess(1);
 	}
+	free(boot);
 	if (lua_pcall(L, 0, -1, 0)) {
 		MessageBoxA(NULL, "moonboot failed to run bootloader", "moonboot diagnostic", 0);
 		ExitProcess(1);
